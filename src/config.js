04ce7811edc2dd6158a48255c119a0264a4c4786
Robert <robertquillaslopez@gmail.com>
2023-02-18 12:18:27 -0500

    vemos

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ const __dirname = path.dirname(__filename)
 dotenv.config({ path:__dirname + '/.env' })
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://robert:04hfDfLOndHtcgd0@cluster0.npkfkpc.mongodb.net/?retryWrites=true&w=majority"
-export const PORT = process.env.PORT 
+export const PORT = process.env.PORT || 5000
 export const KEYSECRETTOKEN = process.env.KEYSECRETTOKEN  || "NOCOUNTRY-PROYECT"
